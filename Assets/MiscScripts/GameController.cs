48dc85de3071e001891a71a366488a5326e68da2
kondorriano <nicobito@gmail.com>
2017-10-22 05:22:29 +0200

    Ended Nav

---
+++ Assets/MiscScripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject navigationPlayer;
+    public GameObject firstScene;
+
+    GameObject navPlayer;
+    GameObject navScene;
+
+	// Use this for initialization
+	void Start () {
+        navScene = Instantiate(firstScene, Vector3.zero, Quaternion.identity);
+        navPlayer = Instantiate(navigationPlayer, Vector3.zero, Quaternion.identity);
+        navPlayer.transform.position = navScene.transform.GetChild(0).GetChild(0).position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/NavigationLogic/NavigationScripts/CharacterMovement.cs
+++ Assets/NavigationLogic/NavigationScripts/CharacterMovement.cs
@@ -9,22 +9,44 @@ public class CharacterMovement : MonoBehaviour {
     public float speed = 5;
     public float speedMod = 1;
     Vector2 walkDirection = Vector2.zero;
+    Animator anim;
 
     private void Awake()
     {
         myRig = GetComponent<Rigidbody2D>();
         sprite = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        anim = transform.GetChild(0).GetComponent<Animator>();
     }
 
 	// Update is called once per frame
 	void Update () {
+        bool up = false;
+        bool down = false;
+        bool right = false;
+        bool left = false;
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
         if (Mathf.Abs(xAxis) < 0.15f) xAxis = 0;
         if (Mathf.Abs(yAxis) < 0.15f) yAxis = 0;
 
-        walkDirection = new Vector3(xAxis, yAxis);
-        myRig.velocity = walkDirection.normalized * speed*speedMod;
+        up = (yAxis >= 0.15f);
+        down = (yAxis <= -0.15f);
+        left = (xAxis <= -0.15f);
+        right = (xAxis >= 0.15f);
+
+        up = up && Mathf.Abs(yAxis) > Mathf.Abs(xAxis);
+        down = down && Mathf.Abs(yAxis) > Mathf.Abs(xAxis);
+        left = left && Mathf.Abs(xAxis) > Mathf.Abs(yAxis);
+        right = right && Mathf.Abs(xAxis) > Mathf.Abs(yAxis);
+
+        walkDirection = (new Vector2(xAxis, yAxis)).normalized;
+        float xSpeedMod = (speedMod <= .2f) ? speedMod * 5f : (speedMod < 1) ? 1 : speedMod;
+        myRig.velocity = new Vector2(xAxis*xSpeedMod, yAxis*speedMod)*speed;
+
+        anim.SetBool("Up", up);
+        anim.SetBool("Down", down);
+        anim.SetBool("Left", left);
+        anim.SetBool("Right", right);
     }
 
     public void SetLayer(int layer)
